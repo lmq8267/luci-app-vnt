a3bd4bd91949c9643b36a3b29521b06d40758116
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2024-01-20 16:55:10 +0800

    Update vnt.lua

--- luci-app-vnt/luasrc/controller/vnt.lua
+++ luci-app-vnt/luasrc/controller/vnt.lua
@@ -21,6 +21,50 @@ function act_status()
 	local e = {}
 	e.crunning = luci.sys.call("pgrep vnt-cli >/dev/null") == 0
 	e.srunning = luci.sys.call("pgrep vnts >/dev/null") == 0
+	local tagfile = io.open("/tmp/vnt_time", "r")
+        if tagfile then
+        local tagcontent = tagfile:read("*all")
+        tagfile:close()
+        if tagcontent and tagcontent ~= "" then
+	local command = io.popen("[ -s /tmp/vnt_time ] && start_time=$(cat /tmp/vnt_time) && time=$(($(date +%s)-start_time)) && day=$((time/86400)) && [ $day -eq 0 ] && day='' || day=${day}天 && time=$(date -u -d @${time} +'%H小时%M分%S秒') && echo $day $time")
+	e.vntsta = command:read("*all")
+	command:close()
+	end
+	end
+        local command2 = io.popen('test ! -z "`pidof vnt-cli`" && (top -b -n1 | grep -E "$(pidof vnt-cli)" 2>/dev/null | grep -v grep | awk \'{for (i=1;i<=NF;i++) {if ($i ~ /vnt-cli/) break; else cpu=i}} END {print $cpu}\')')
+	e.vntcpu = command2:read("*all")
+	command2:close()
+        local command3 = io.popen("test ! -z `pidof vnt-cli` && (cat /proc/$(pidof vnt-cli | awk '{print $NF}')/status | grep -w VmRSS | awk '{printf \"%.2f MB\", $2/1024}')")
+	e.vntram = command3:read("*all")
+	command3:close()
+	local stagfile = io.open("/tmp/vnts_time", "r")
+        if stagfile then
+        local stagcontent = stagfile:read("*all")
+        stagfile:close()
+        local command4 = io.popen("[ -s /tmp/vnts_time ] && start_time=$(cat /tmp/vnts_time) && time=$(($(date +%s)-start_time)) && day=$((time/86400)) && [ $day -eq 0 ] && day='' || day=${day}天 && time=$(date -u -d @${time} +'%H小时%M分%S秒') && echo $day $time")
+	e.vntsta2 = command4:read("*all")
+	command4:close()
+	end
+        end
+        local command5 = io.popen('test ! -z "`pidof vnts`" && (top -b -n1 | grep -E "$(pidof vnts)" 2>/dev/null | grep -v grep | awk \'{for (i=1;i<=NF;i++) {if ($i ~ /vnts/) break; else cpu=i}} END {print $cpu}\')')
+	e.vntscpu = command5:read("*all")
+	command5:close()
+        local command6 = io.popen("test ! -z `pidof vnts` && (cat /proc/$(pidof vnts | awk '{print $NF}')/status | grep -w VmRSS | awk '{printf \"%.2f MB\", $2/1024}')")
+	e.vntsram = command6:read("*all")
+	command6:close()
+	local command7 = io.popen("([ -s /tmp/vnt.tag ] && cat /tmp/vnt.tag ) || (echo `$(uci -q get vnt.@vnt-cli[0].clibin) -h |grep 'version:'| awk -F 'version:' '{print $2}'` > /tmp/vnt.tag && cat /tmp/vnt.tag)")
+	e.vnttag = command7:read("*all")
+	command7:close()
+        local command8 = io.popen("([ -s /tmp/vntnew.tag ] && cat /tmp/vntnew.tag ) || ( curl -L -k -s --connect-timeout 3 --user-agent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36' https://api.github.com/repos/lbl8603/vnt/releases/latest | grep tag_name | sed 's/[^0-9.]*//g' >/tmp/vntnew.tag && cat /tmp/vntnew.tag )")
+	e.vntnewtag = command8:read("*all")
+	command8:close()
+        local command9 = io.popen("([ -s /tmp/vnts.tag ] && cat /tmp/vnts.tag ) || ( echo `$(uci -q get vnt.@vnts[0].vntsbin) -V | sed 's/[^0-9.]*//g'` > /tmp/vnts.tag && cat /tmp/vnts.tag && [ -s /tmp/vnts.tag ] && echo '？' >> /tmp/vnts.tag && cat /tmp/vnts.tag )")
+	e.vntstag = command9:read("*all")
+	command9:close()
+        local command0 = io.popen("([ -s /tmp/vntsnew.tag ] && cat /tmp/vntsnew.tag ) || ( curl -L -k -s --connect-timeout 3 --user-agent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36' https://api.github.com/repos/lbl8603/vnts/releases/latest | grep tag_name | sed 's/[^0-9.]*//g' >/tmp/vntsnew.tag ; cat /tmp/vntsnew.tag )")
+	e.vntsnewtag = command0:read("*all")
+	command0:close()
+
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(e)
 end
